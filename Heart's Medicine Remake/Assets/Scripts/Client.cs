90329d6b7a3f2f8d6d1817cbf3198dbbe8bc7958
Joanna <32140567+joannasoltysiak@users.noreply.github.com>
2019-12-06 23:11:02 +0100

    enum ClientState
    plus comments

--- Heart's Medicine Remake/Assets/Scripts/Client.cs
+++ Heart's Medicine Remake/Assets/Scripts/Client.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Client : MonoBehaviour
 {
+    public 
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +18,13 @@ public class Client : MonoBehaviour
     {
         
     }
+}
+
+public enum ClientState
+{
+    Walking,                //action done
+    WaitingToBePlaced,      //after reaching this state the first timer goes on
+    WaitingForAction,       //after being in the right place the timer for action
+    WaitingToPay,           //no timer, last state
+
 }
