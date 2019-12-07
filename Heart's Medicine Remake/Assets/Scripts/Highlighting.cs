85090e87edd9aa254a3ab6be306727830ef8431b
Joanna <32140567+joannasoltysiak@users.noreply.github.com>
2019-12-07 20:17:03 +0100

    Highlight
    To see what we can click

---
+++ Heart's Medicine Remake/Assets/Scripts/Highlighting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlighting : MonoBehaviour
+{
+    private Renderer rend;
+    private Color colorNow;
+
+    [SerializeField]
+    private Color colorToTurnTo = Color.white;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+
+        colorNow = rend.material.color;
+    }
+
+    void OnMouseOver()
+    {
+        rend.material.color = colorToTurnTo;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = colorNow;
+    }
+}
