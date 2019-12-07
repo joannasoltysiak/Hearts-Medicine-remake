58ee8ee5169289739de6e0b725208afc1c3ccf8b
UminoAoi <48631554+UminoAoi@users.noreply.github.com>
2019-12-07 11:40:47 +0100

    Trying A*

---
+++ Heart's Medicine Remake/Assets/Scripts/MoveTarget.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTarget : MonoBehaviour
+{
+
+    public LayerMask hitLayers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mouse = Input.mousePosition;
+            Ray castPoint = Camera.main.ScreenPointToRay(mouse); // cast ray where mouse is pointing at
+            RaycastHit hit; //stores the position where ray hit
+            if (Physics.Raycast(castPoint, out hit, Mathf.Infinity, hitLayers))
+            {
+                this.transform.position = hit.point;
+            }
+        }
+    }
+}
