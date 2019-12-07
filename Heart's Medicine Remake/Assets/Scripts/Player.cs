87900f80a9e940b0b64f22a9ec6a77032cce2316
UminoAoi <48631554+UminoAoi@users.noreply.github.com>
2019-12-07 13:03:02 +0100

    Item script, Player script

---
+++ Heart's Medicine Remake/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Item[] itemList;
+    Vector2 currentPosition;
+    Vector2 targetPosition;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemList = new Item[3];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
