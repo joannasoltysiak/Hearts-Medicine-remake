b2b37f46ade3d6a57d1798b2cbb8ad04aa8e4342
UminoAoi <48631554+UminoAoi@users.noreply.github.com>
2019-12-07 13:48:16 +0100

    Updated GameState

--- Heart's Medicine Remake/Assets/Scripts/GameState.cs
+++ Heart's Medicine Remake/Assets/Scripts/GameState.cs
@@ -17,12 +17,24 @@ public class GameState : MonoBehaviour
             switch (hit.collider.tag)
             {
                 case "Client":
+                    clickedPlace = Vector2.zero;
                     clickedObject = hit.collider.gameObject;
+                    //make client active
+                    break;
+                case "Item":
+                    clickedObject = hit.collider.gameObject;
+                    clickedPlace = hit.transform.position;
+                    //set players target and when position is okay, take item
                     break;
                 case "ActionPlace":
+                    clickedPlace = hit.transform.position;
+                    //set active client target to this, then move client
+                    clickedObject = null;
                     break;
                 case "Floor":
+                    clickedObject = null;
                     clickedPlace = hit.transform.position;
+                    //set player's target to this, in update move player
                     break;
                 default:
                     break;
