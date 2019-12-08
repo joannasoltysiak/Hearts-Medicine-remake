f6411c0c4236c66d7cf1f874f0cb5f2d5bcfda9b
Joanna <32140567+joannasoltysiak@users.noreply.github.com>
2019-12-08 15:01:31 +0100

    Adding Reception code
    For reception - where customers will stand in line and after taking action, points will be calculated based on customer happiness XD

---
+++ Heart's Medicine Remake/Assets/Scripts/Reception.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reception : MonoBehaviour
+{
+    List<Client> clients;   //this will retain data about customers standing in line
+
+    void Start()
+    {
+        clients = new List<Client>();
+    }
+
+    public void AddNewClient(Client client)
+    {
+        clients.Add(client);
+    }
+
+    public void GetPoints()
+    {
+        foreach (Client c in clients)
+        {
+            GameState.points += 10;
+        }
+
+        clients.Clear();
+    }
+
+}
