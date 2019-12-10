aeb38a27a3baaad039ece145a94e8a0dbdd1ccd2
UminoAoi <48631554+UminoAoi@users.noreply.github.com>
2019-12-10 15:14:49 +0100

    Making waiting room

--- Heart's Medicine Remake/Assets/Scripts/Timer.cs
+++ Heart's Medicine Remake/Assets/Scripts/Timer.cs
@@ -8,12 +8,14 @@ public class Timer : MonoBehaviour
     Image timerBar;
     public float maxTime = 5f;
     float timeLeft;
+    float nextClient;
 
     // Start is called before the first frame update
     void Start()
     {
         timerBar = GetComponent<Image>();
         timeLeft = maxTime;
+        nextClient = 1f;
     }
 
     // Update is called once per frame
@@ -23,6 +25,13 @@ public class Timer : MonoBehaviour
         {
             timeLeft -= Time.deltaTime;
             timerBar.fillAmount = timeLeft / maxTime;
+            nextClient += Time.deltaTime;
+
+            if(nextClient > 1f)
+            {
+                WaitingRoom.SpawnClient();
+                nextClient = 0;
+            }
         }
         else
         {
---
+++ Heart's Medicine Remake/Assets/Scripts/WaitingRoom.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitingRoom
+{
+    static Vector2 place1 = new Vector2(-4, -3);
+    static Vector2 place2 = new Vector2(-6, -3);
+    static Vector2 place3 = new Vector2(-8, -3);
+
+    public static GameObject client;
+
+    static List<GameObject> clients;
+
+    public static void SpawnClient()
+    {
+        switch (clients.Count)
+        {
+            case 0:
+                client.transform.position = place1;
+                Object.Instantiate(client, client.transform);
+                clients.Add(client);
+                break;
+            case 1:
+                client.transform.position = place2;
+                Object.Instantiate(client, client.transform);
+                clients.Add(client);
+                break;
+            case 2:
+                client.transform.position = place3;
+                Object.Instantiate(client, client.transform);
+                clients.Add(client);
+                break;
+            default:
+                Debug.Log("You lost a client LOL");
+                break;
+        }
+    }
+
+    public void DeleteClient(GameObject client)
+    {
+        clients.Remove(client);
+    }
+
+}
